bb0bf1182c6b7258f9f82104feb4b5b402967899
virajraput <virajrajput7861ag@gmail.com>
2026-05-08 17:13:29 +0530

    Quetion 2

---
+++ largest-in-array/Q2.js
@@ -0,0 +1,21 @@
+// Given an array arr[]. The task is to find the largest element and return it.
+
+// Examples:
+
+// Input: arr[] = [1, 8, 7, 56, 90]
+// Output: 90
+// Explanation: The largest element of the given array is 90.
+
+// Quetions link : (https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1?page=1&category=Arrays&difficulty=Basic&sortBy=submissions)
+
+function largest(arr) {
+
+        let biger = arr[0]
+        for(let i=0; i< arr.length ; i++){
+            if(arr[i]>biger){
+                biger=arr[i]
+            }
+        }
+        console.log(biger);
+    }
+largest([1, 8, 7, 56, 90]);
