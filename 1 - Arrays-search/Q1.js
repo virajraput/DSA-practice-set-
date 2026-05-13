edd4c9e43699d086f9db2c94b1fc2c24b531a0c7
virajraput <virajrajput7861ag@gmail.com>
2026-05-13 16:28:45 +0530

    Question 1

--- 1 - Arrays-search/Q1.js
+++ 1 - Arrays-search/Q1.js
@@ -5,7 +5,7 @@
 // Input: arr[] = [1, 2, 3, 4], x = 3
 // Output: 2
 // Explanation: For array [1, 2, 3, 4], the element to be searched is 3. Since 3 is present at index 2, the output is 2.
-// Quetion link : (https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1?page=1&category=Arrays&difficulty=Basic&sortBy=submissions)
+// Question link : (https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1?page=1&category=Arrays&difficulty=Basic&sortBy=submissions)
 
 
  function search(arr, x) {
